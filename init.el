fef026eda102941fc5c9654e55d940ef50698862
Graham Hughes <graehu@gmail.com>
2020-08-11 23:12:34 +0100

    More packages
    ace jump magit and yasnippet. Also set a better default stype for c
    and enabled eglot for python. Ansi colours in compile mode + follow by
    default.

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "2642a1b7f53b9bb34c7f1e032d2098c852811ec2881eec2dc8cc07be004e45a0" "b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" default)))
  '(package-selected-packages
    (quote
-    (yasnippet markdown-mode disaster spacemacs-theme multiple-cursors helm-projectile atom-dark-theme company rainbow-delimiters helm one-themes ## which-key eglot))))
+    (magit ace-jump-mode yasnippet markdown-mode disaster spacemacs-theme multiple-cursors helm-projectile atom-dark-theme company rainbow-delimiters helm one-themes ## which-key eglot))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,11 +41,20 @@ There are two things you can do about this warning:
 (package-initialize)
 ;;tings.
 (load-theme 'spacemacs-dark)
+;; ansi colours in compilation mode
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region compilation-filter-start (point))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+(setq compilation-scroll-output t)
 ;; find some way to start eglot server when entering a project
 (require 'eglot)
 ;; Language hooks
 ;; C/C++
 (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"));"--extra-arg-before=-xc++"));"--log=verbose"
+(setq c-default-style "ellemtel")
 (add-hook 'c++-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'electric-pair-mode)
 (add-hook 'c++-mode-hook 'show-paren-mode)
@@ -55,6 +64,9 @@ There are two things you can do about this warning:
 ;; Python
 (add-hook 'python-mode-hook 'eglot-ensure)
 (add-hook 'python-mode-hook 'electric-pair-mode)
+;; Python
+(add-hook 'javascript-mode-hook 'eglot-ensure)
+(add-hook 'javascript-mode-hook 'electric-pair-mode)
 ;; Emacs lisp
 (add-hook 'emacs-lisp-mode-hook 'electric-pair-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
@@ -73,6 +85,7 @@ There are two things you can do about this warning:
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x m") 'helm-global-mark-ring)
 (helm-mode 1)
 ;; Goto changes
 (global-set-key (kbd "M-g n") 'flymake-goto-next-error)
@@ -87,6 +100,14 @@ There are two things you can do about this warning:
 (define-key mc-map (kbd "n") 'mc/skip-to-next-like-this)
 (define-key mc-map (kbd "C-p") 'mc/mark-previous-like-this)
 (define-key mc-map (kbd "p") 'mc/skip-to-previous-like-this)
+(define-key mc-map (kbd "C-j") 'ace-jump-mode)
+;; (add-to-list 'load-path "/full/path/where/ace-jump-mode.el/in/")
+;; (autoload
+;;   'ace-jump-mode
+;;   "ace-jump-mode"
+;;   "Emacs quick move minor mode"
+;;   t)
+;; you can select the key you prefer to
 
 ;;
 (provide '.emacs)
