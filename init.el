1b6ed3d81e7e4f4c83759490b9cd2f92d7266394
Graham Hughes <graehu@gmail.com>
2021-05-09 08:47:47 +0100

    dim avy jumps

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(avy-background t)
  '(company-backends
    (quote
     (company-bbdb company-eclim company-semantic company-xcode company-cmake company-capf company-files
